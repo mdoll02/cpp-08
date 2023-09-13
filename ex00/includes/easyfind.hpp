97c8575368d499e8a8888b4ca25eac95f11a5e06
mdoll <mdoll@student.42wolfsburg.de>
2023-09-13 09:07:07 +0200

    finished ex00

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -10,8 +10,8 @@ public:
 };
 
 template <typename T>
-typename T::iterator easyfind(T &container, int to_find) {
-	 typename T::iterator it = std::find(container.begin(), container.end(), to_find);
+typename T::const_iterator easyfind(T &container, int to_find) {
+	 typename T::const_iterator it = std::find(container.begin(), container.end(), to_find);
 	 if (it == container.end())
 		throw NotFoundException();
 	return it;
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,8 +1,46 @@
 #include "Colors.hpp"
 #include <iostream>
+#include <vector>
+#include <list>
 #include "easyfind.hpp"
 
 int main() {
-	std::cout << GREEN << "Hello World!" << R << std::endl;
+	std::vector<int> vi(10);
+
+	for (int i = 0; i < 10; i++)
+		vi.push_back(i);
+	try {
+		::easyfind(vi, 5);
+		std::cout << GREEN << "Found 5 in vi" << R << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << RED << "ERROR: " << e.what() << R << std::endl;
+	}
+	try {
+		::easyfind(vi, 99);
+		std::cout << GREEN << "Found 99 in vi" << R << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << RED << "ERROR: " << e.what() << R << std::endl;
+	}
+
+	std::list<int> li(10);
+
+	for (int i = 0; i < 10; i++)
+		li.push_back(i);
+	try {
+		::easyfind(li, 3);
+		std::cout << GREEN << "Found 3 in li" << R << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << RED << "ERROR: " << e.what() << R << std::endl;
+	}
+	try {
+		::easyfind(li, 19023);
+		std::cout << GREEN << "Found 19023 in li" << R << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << RED << "ERROR: " << e.what() << R << std::endl;
+	}
 	return 0;
 }
